4890ee4af665fdcd7f72bb30f5fff4aa480d45e4
Sasha <warmsasha@gmail.com>
2019-08-21 19:47:54 +0400

    Скрывает переключатель в навигации, когда JS не работает

--- source/js/button.js
+++ source/js/button.js
@@ -2,6 +2,7 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navToggle.classList.add('main-nav__toggle--show');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
