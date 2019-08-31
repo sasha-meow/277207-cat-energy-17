8b448bc7046b8a873498f9e8761797c9fc1f206e
Sasha <warmsasha@gmail.com>
2019-08-31 19:24:46 +0400

    Вносит правки в разметку и правит всё, что с этим связано на главной странице

--- gulpfile.js
+++ gulpfile.js
@@ -32,13 +32,13 @@ gulp.task("css", function () {
 });
 
 gulp.task("images", function () {
- return gulp.src("source/img/**/*.{png,jpg,svg}")
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true}),
-    imagemin.svgo()
-  ]))
-  .pipe(gulp.dest("source/img"));
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("webp", function () {
