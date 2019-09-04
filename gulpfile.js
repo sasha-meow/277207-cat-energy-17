67302d85252b9c464df3b8f1797f41c7045803d0
Sasha <warmsasha@gmail.com>
2019-09-04 23:05:10 +0400

    Вносит правки

--- gulpfile.js
+++ gulpfile.js
@@ -88,6 +88,7 @@ gulp.task("copy", function () {
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
       "source/js/**",
+      "source/css/**",
       "source/*.ico"
     ], {
       base: "source"
